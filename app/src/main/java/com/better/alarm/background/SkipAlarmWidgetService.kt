fa0e0b0ee5c57a02edff84aa12874cfe369ca2d2
Juan V <jjsiete@gmail.com>
2022-04-10 00:02:23 +0200

    move the skip widget by dragging it

--- app/src/main/java/com/better/alarm/background/SkipAlarmWidgetService.kt
+++ app/src/main/java/com/better/alarm/background/SkipAlarmWidgetService.kt
@@ -1,16 +1,16 @@
 package com.better.alarm.background
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.WindowManager
+import android.util.Log
+import android.view.*
 import android.widget.TextView
+import androidx.core.view.GestureDetectorCompat
 import com.better.alarm.R
 import com.better.alarm.interfaces.Intents
 import com.better.alarm.interfaces.PresentationToModelIntents
@@ -25,6 +25,7 @@ class SkipAlarmWidgetService : Service() {
     private lateinit var widgetFloatingView: View
     private lateinit var windowManager: WindowManager
     private var layoutFlag = 0
+    private lateinit var mGestureDetector: GestureDetectorCompat
 
     override fun onBind(p0: Intent?): IBinder {
         return binder
@@ -56,22 +57,23 @@ class SkipAlarmWidgetService : Service() {
             widgetText += "\n" + alarmLabel
         }
         widgetTextView.setText(widgetText)
-        widgetTextView.setOnClickListener{
-            val pendingSkip =
-                PresentationToModelIntents.createPendingIntent(
-                    this,
-                    PresentationToModelIntents.ACTION_REQUEST_SKIP,
-                    alarmId,
-                    widgetText
-                )
-            pendingSkip.send()
-        }
 
         windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
         windowManager.apply {
             addView(widgetFloatingView, layoutParams)
         }
         widgetFloatingView.visibility = View.VISIBLE
+
+        mGestureDetector = GestureDetectorCompat(
+            this,
+            TouchAndScrollGestureListener(windowManager, layoutParams, widgetFloatingView, this, alarmId, widgetText)
+        )
+
+        widgetFloatingView.setOnTouchListener { _, event ->
+            mGestureDetector.onTouchEvent(event)
+            true
+        }
+
         return START_STICKY
     }
 
@@ -79,5 +81,52 @@ class SkipAlarmWidgetService : Service() {
         super.onDestroy()
         if (widgetFloatingView != null)
             windowManager.removeView(widgetFloatingView)
+    }
+}
+
+private class TouchAndScrollGestureListener(
+    val windowManager: WindowManager,
+    val layoutParams: WindowManager.LayoutParams,
+    val widgetFloatingView: View,
+    val context: Context,
+    val alarmId: Int,
+    val widgetText: String
+) :
+    GestureDetector.SimpleOnGestureListener() {
+    private var initialX: Int = 0
+    private var initialY: Int = 0
+    private var initialTouchX: Float = 0f
+    private var initialTouchY: Float = 0f
+
+    override fun onDown(e: MotionEvent): Boolean {
+        initialX = layoutParams.x
+        initialY = layoutParams.y
+        initialTouchX = e.rawX
+        initialTouchY = e.rawY
+        return true
+    }
+
+    override fun onSingleTapUp(event: MotionEvent): Boolean {
+        val pendingSkip =
+                PresentationToModelIntents.createPendingIntent(
+                    context,
+                    PresentationToModelIntents.ACTION_REQUEST_SKIP,
+                    alarmId,
+                    widgetText
+                )
+            pendingSkip.send()
+        return true
+    }
+
+    override fun onScroll(
+        initialEvent: MotionEvent,
+        finalEvent: MotionEvent,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        layoutParams.x = initialX + (initialTouchX-finalEvent.rawX).toInt()
+        layoutParams.y = initialY + (finalEvent.rawY-initialTouchY).toInt()
+        windowManager.updateViewLayout(widgetFloatingView, layoutParams)
+        return true
     }
 }
